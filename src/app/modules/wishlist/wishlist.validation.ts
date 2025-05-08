0f076236eccd565e49d38770e16e4c589c7ba887
shahadathhs <shahadathhossensajib732@gmail.com>
2025-05-08 07:50:11 +0600

    fix: fixed wishlist validation schama

--- src/app/modules/wishlist/wishlist.validation.ts
+++ src/app/modules/wishlist/wishlist.validation.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 const wishlistSchema = z.object({
   body: z.object({
-    bikeId: z.string({ required_error: 'Bike ID is required' }),
+    listingId: z.string({ required_error: 'Listing ID is required' }),
     action: z.enum(['add', 'remove'], { required_error: 'Action is required' })
   })
 })
